77606b10364c783f8992a153ed00f7f4a588f9ec
Ronjundi <fox.contas@hotmail.com>
2022-11-22 16:39:01 -0300

    added gallery section

--- guilded-website/pages/components/gallery.js
+++ guilded-website/pages/components/gallery.js
@@ -4,6 +4,57 @@ function Gallery(){
     return(
      <div className="gallery-container">
 
+        <h1>Gallery</h1>
+
+
+        <div className="gallery-content-container">
+
+
+        
+
+        <div className="screenshots-container">
+        </div>
+
+        <div className="gallery-right-side">
+
+
+
+        <div className="gallery-box2">
+
+        <div className="styles-container">
+        </div>
+        <div className="uniform-container">
+        </div>
+        
+        </div>
+
+        
+        <div className="gallery-box3">
+        
+        <div className="guides-container">
+        </div>        
+        <div className="gear-container">
+        </div>
+        <div className="lifeskills-container">
+        </div>
+
+        </div>
+        
+        </div>
+
+
+
+
+            
+
+        
+       
+
+
+        </div>
+
+
+        
      
 
 
